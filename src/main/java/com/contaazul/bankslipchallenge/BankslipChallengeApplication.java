57606840b7688ea8004a1e4540c3dfc090a2e78a
stefanoglaz <stefanog.laz@gmail.com>
2021-01-01 14:57:36 -0300

    first commit

---
+++ src/main/java/com/contaazul/bankslipchallenge/BankslipChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.contaazul.bankslipchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankslipChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankslipChallengeApplication.class, args);
+	}
+
+}
